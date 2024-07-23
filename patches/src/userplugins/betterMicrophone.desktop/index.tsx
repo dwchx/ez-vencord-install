4914f0495658c93670e018b5e920d53080bd48a9
dwchx <dwchx@users.noreply.github.com>
2024-07-23 16:10:55 +0000

    Pulled files from https://github.com/philhk/vencord.

--- patches/src/userplugins/betterMicrophone.desktop/index.tsx
+++ patches/src/userplugins/betterMicrophone.desktop/index.tsx
@@ -1,21 +1,37 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { PluginAuthor, PluginDef } from "@utils/types";
+
 import { addSettingsPanelButton, Emitter, MicrophoneSettingsIcon, removeSettingsPanelButton } from "../philsPluginLibrary";
 import { PluginInfo } from "./constants";
 import { openMicrophoneSettingsModal } from "./modals";
 import { MicrophonePatcher } from "./patchers";
 import { initMicrophoneStore } from "./stores";
 
-// Main Plugin Class Implementation
 export default new class Plugin implements PluginDef {
     readonly name: string;
     readonly description: string;
     readonly authors: PluginAuthor[];
     readonly dependencies: string[];
 
-    // Optional property to store the microphone patcher instance
     public microphonePatcher?: MicrophonePatcher;
 
-    // Constructor initializes plugin information from PluginInfo constants
     constructor() {
         this.name = PluginInfo.PLUGIN_NAME;
         this.description = PluginInfo.DESCRIPTION;
@@ -23,32 +39,19 @@ export default new class Plugin implements PluginDef {
         this.dependencies = ["PhilsPluginLibrary"];
     }
 
-    // Method to start the plugin
     start(): void {
-        // Initialize the microphone store
         initMicrophoneStore();
 
-        // Apply microphone patching
         this.microphonePatcher = new MicrophonePatcher().patch();
 
-        // Add settings panel button for microphone settings
-        addSettingsPanelButton({
-            name: PluginInfo.PLUGIN_NAME,
-            icon: MicrophoneSettingsIcon,
-            tooltipText: "Microphone Settings",
-            onClick: openMicrophoneSettingsModal
-        });
+        addSettingsPanelButton({ name: PluginInfo.PLUGIN_NAME, icon: MicrophoneSettingsIcon, tooltipText: "Microphone Settings", onClick: openMicrophoneSettingsModal });
     }
 
-    // Method to stop the plugin
     stop(): void {
-        // Unpatch the microphone patcher if it exists
         this.microphonePatcher?.unpatch();
 
-        // Remove all event listeners associated with this plugin
         Emitter.removeAllListeners(PluginInfo.PLUGIN_NAME);
 
-        // Remove the settings panel button
         removeSettingsPanelButton(PluginInfo.PLUGIN_NAME);
     }
 };
