ed966884c9e1237851c917881170c41b20e6552a
dwchx <dwchx@users.noreply.github.com>
2024-07-23 16:10:37 +0000

    Pulled files from https://github.com/philhk/vencord.

--- patches/src/userplugins/betterMicrophone.desktop/constants/constants.ts
+++ patches/src/userplugins/betterMicrophone.desktop/constants/constants.ts
@@ -1,23 +1,31 @@
-// Importing required modules and constants
-import { Devs } from "@utils/constants"; // Importing developer information from the constants module
-import { types } from "../../philsPluginLibrary"; // Importing types from the plugin library
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
-// Define and export the PluginInfo object with relevant plugin information
+import { Devs } from "@utils/constants";
+
+import { types } from "../../philsPluginLibrary";
+
 export const PluginInfo = {
-    // Name of the plugin
     PLUGIN_NAME: "BetterMicrophone",
-    
-    // Description of what the plugin does
     DESCRIPTION: "This plugin allows you to further customize your microphone.",
-    
-    // Information about the author
     AUTHOR: {
-        // Spread operator to include details from the philhk developer object
         ...Devs.philhk,
-        // GitHub link for the author
         github: "https://github.com/philhk"
     },
-    
-    // Contributors to the plugin (currently empty)
     CONTRIBUTORS: {}
-} as const satisfies types.PluginInfo; // Type assertion to ensure PluginInfo adheres to the PluginInfo type
+} as const satisfies types.PluginInfo;
